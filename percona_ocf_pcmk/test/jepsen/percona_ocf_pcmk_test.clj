b3677e516d7c8125a7528c7cbb5ec89146113617
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-05-16 16:12:06 +0200

    Add tests for RR/lock-less
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- percona_ocf_pcmk/test/jepsen/percona_ocf_pcmk_test.clj
+++ percona_ocf_pcmk/test/jepsen/percona_ocf_pcmk_test.clj
@@ -15,5 +15,13 @@
 (deftest bank-test-multi
   (is (:valid? (:results (run! (bank-test 2 10 " FOR UPDATE" false rand-nth :serializable))))))
 
+; the test for a lock-less SELECT and RR transactions, which is default mode
+(deftest bank-test-multi-rr
+  (is (:valid? (:results (run! (bank-test 2 10 "" false rand-nth :repeatable-read))))))
+
+; the test against dirty reads and A5A read skews
 (deftest dirty-reads-test
   (is (:valid? (:results (run! (dirty-reads/test- 4 rand-nth :serializable))))))
+
+(deftest dirty-reads-test-rr
+  (is (:valid? (:results (run! (dirty-reads/test- 4 rand-nth :repeatable-read))))))
